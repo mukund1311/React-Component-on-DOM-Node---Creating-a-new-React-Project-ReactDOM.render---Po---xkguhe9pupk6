be9083a1dde70710a535161c610dfee65e5a6d1b
Mukund Patel <80103805+mukund1311@users.noreply.github.com>
2022-12-16 08:10:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,18 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+
+
+import React from "react";
+
+
+const App = () => {
+  return (
+    <>
+    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+     <div id="root">I am a App</div>
+    </>
+   )
+}
+
+
+export default App;
+
 
